8a4ea48b7dcdc92edaae6905c4d106a637f1b017
URIAN VIERA <urian1213viera@gmail.com>
2025-03-03 09:04:00 -0500

    agregando imagenes

--- app-contacts/src/App.jsx
+++ app-contacts/src/App.jsx
@@ -13,6 +13,18 @@ function App() {
     <>
       <NavBar />
       <div className="container mt-3">
+        <div className="row justify-content-center">
+          <div className="col-md-12 d-flex justify-content-center align-items-center gap-3">
+            <img src="/img/react.svg" alt="React" width={100} />
+            <span>+</span>
+            <img src="/img/python.webp" alt="Python" width={100} />
+            <span>+</span>
+            <img src="/img/django.webp" alt="Django" width={100} />
+            <span>+</span>
+            <img src="/img/mysql.webp" alt="MySQL" width={80} />
+          </div>
+        </div>
+
         <div className="row">
           <div className="col-md-5 mt-5">
             <Formulario />
--- app-contacts/src/components/NavBar.jsx
+++ app-contacts/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="container-fluid d-flex justify-content-center">
-        <h1>CRUD FullStack Reactjs + Python + Django + MySQL</h1>
+        <h1>CRUD FullStack con React, Python, Django (DRF) y MySQL</h1>
       </div>
     </nav>
   );
